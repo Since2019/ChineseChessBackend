99b1c60c46b6d06486f4cfe67475631d4dd85fb5
Rickson2019 <JudaoZhong@gmail.com>
2022-07-10 08:26:55 -0700

    Added cors() and made changes to adpt to the use of socket.io

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,56 @@
 import express from 'express'
 import routes from './routes';
 
-/*
-Dummy App right now for the chess backend
-**/
+import {
+    SocketIoServer,
+    Socket
+} from "./utils/socket" 
+
+import http from "http"; // SocketIoServer accepts a type of http.Server
+
+import cors from 'cors' // to get rid of Cross-Origin-Not-Allowed issues    
+
+
+// import { Server, Socket } from "socket.io";
+
+
+const allowedOrigins = ['*'];
+const options: cors.CorsOptions = {
+    origin: allowedOrigins
+};
+    
+ 
 class App {
-    public server;
+    private expressServer; // http server that handles the api endpoints.
+    private socketServer;  // socket.io server that lives on top of express server
+    public server;         // http server that accepted the express object, which would host both express apis and the socket.io utilities.
 
     constructor () {
-        this.server = express();
+        // Express Server uses cors and will be wrapped by http.
+        this.expressServer = express().use(cors(options)); 
+
+        // instantiate the socket server upon express server
+        this.socketServer = new SocketIoServer(this.server, {
+            cors: {
+                origin : "*",
+                methods :["GET","POST"]
+            }
+        });
+
+        this.server = new http.Server(this.expressServer)
+
 
         this.middlewares();
-        this.routes();
+        this.routes();  
+        
     }
 
     middlewares () {
-        this.server.use(express.json());
+        this.expressServer.use(express.json());
     }
 
     routes () {
-        this.server.use(routes)
+        this.expressServer.use(routes)
     }
 }
 
